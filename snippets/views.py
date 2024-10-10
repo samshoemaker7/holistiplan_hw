0aed07f14f5c50f13716dd0824472e6457ac72da
Sam Shoemaker <sshoemaker@protoplay.tv>
2024-10-10 18:10:07 -0400

    Make hw RESTful

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from rest_framework import generics, permissions, renderers
+from rest_framework import generics, permissions, renderers, viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
@@ -28,24 +28,6 @@ def api_root(request, format=None):
     )
 
 @api_view(["POST"])
-@permission_classes([IsAdminUser])
-def create_user(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-
-    user = ExtendedUser.objects.create_user(username=username, password=password)
-    return Response({ "OK": "OK" })
-
-@api_view(["DELETE"])
-@permission_classes([IsAdminUser])
-def delete_user(request):
-    username = request.data.get('username')
-    user = ExtendedUser.objects.filter(username=username).first()
-    user.soft_delete()
-    user.save()
-    return Response({ "OK": "OK" })        
-
-@api_view(["POST"])
 def login_user(request):
     username = request.data.get('username')
     password = request.data.get('password')
@@ -71,21 +53,91 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
         IsOwnerOrReadOnly,
     )  
 
-class UserList(generics.ListAPIView):
+class UserList(generics.ListCreateAPIView):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        if self.request.user.is_staff and self.request.GET.get('show_deleted'):
-            return ExtendedUser.objects.all()
-        return ExtendedUser.objects.filter(is_deleted=False)            
+        is_staff = self.request.user.is_staff
+        show_deleted = self.request.GET.get('show_deleted')
+        
+        queryset = ExtendedUser.objects.filter(is_deleted=False)
+        if is_staff and show_deleted:
+            queryset = ExtendedUser.objects.all()
 
-class UserDetail(generics.RetrieveAPIView):
+        return queryset
+    
+    # Get a list of all users
+    def get(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        
+        page = self.paginate_queryset(queryset)
+        if page:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        return Response({
+            'count': 0,
+            'next': None,
+            'previous': None,
+            'results': []
+        })
+    
+    # Create a new user
+    def post(self, request):
+        is_staff = request.user.is_staff
+        if not is_staff:
+            return Response(
+                {
+                    "message": "Authentication credentials were not provided."
+                },
+                status = status.HTTP_401_UNAUTHORIZED
+            )
+        
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response(
+                {
+                    "message": "Missing username and/or password."
+                },
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+
+        user = ExtendedUser.objects.create_user(username=username, password=password)
+        return Response(
+            { 
+                "message": "New user was created."
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+class UserDetail(generics.RetrieveDestroyAPIView):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        if self.request.user.is_staff and self.request.GET.get('show_deleted'):
+        is_staff = self.request.user.is_staff
+        show_deleted = self.request.GET.get('show_deleted')
+        if is_staff and show_deleted:
             return ExtendedUser.objects.all()
-        return ExtendedUser.objects.filter(is_deleted=False)  
+        return ExtendedUser.objects.filter(is_deleted=False)
+
+    # Soft delete a user
+    def delete(self, request, *args, **kwargs):
+        is_staff = request.user.is_staff
+        if not is_staff:
+            return Response(
+                {
+                    "message": "Authentication credentials were not provided."
+                },
+                status = status.HTTP_401_UNAUTHORIZED
+            )
+        
+        primary_key = kwargs['pk']
+        user = ExtendedUser.objects.filter(pk=primary_key).first()
+        user.soft_delete()
+        user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class AuditList(generics.ListAPIView):
     queryset=Audit.objects.all()
